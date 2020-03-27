9ca160fbd57a6662fcf3aba8829c5f4c88c46d52
Fizza Usman <52672094+codes-by-kiwi@users.noreply.github.com>
2020-03-27 12:22:59 +0100

    Add files via upload

---
+++ if-1.py
@@ -0,0 +1,12 @@
+x = 4 #change this to see how the code works
+if x == 1:  #this is the syntax of an if statement in python 
+    # indented four spaces
+    print("x is 1.")
+
+else:
+    # indented four spaces
+    print("x is something else")
+
+
+
+# the indentation in python is important to see what is contained within something
